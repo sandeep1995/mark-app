2023e1c9c9ccfa69bd2267d368b93f5440da33cd
sandeep1995 <i.am.sandeep.acharya@gmail.com>
2023-06-06 01:33:14 +0530

    feat: basic markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className='max-w-full mx-auto'>
+        <nav className='flex items-center justify-between flex-wrap bg-indigo-700 px-8 py-4'>
+          <div className='flex items-center flex-shrink-0 text-white mr-6'>
+            <span className='font-semibold text-xl tracking-tight'>
+              MarkerX
+            </span>
+          </div>
+        </nav>
+      </header>
+      <main className='max-w-8xl mx-auto'>
+        <div className='grid w-full sm:grid-cols-6 min-h-screen'>
+          <div className='sm:col-span-4 bg-white p-4'></div>
+          <div className='sm:col-span-2 bg-slate-100 p-4'></div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
